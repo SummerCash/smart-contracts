a422b3c2fcd39de4e37e30bf01809073d6d00ef1
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-21 15:23:55 -0500

    Init crate.

---
+++ chat_board/target/rls/debug/deps/chat_board-77483abcf2f1a016.d
@@ -0,0 +1,5 @@
+/Users/dowlandaiello/rust/rust-smart-contracts/chat_board/target/rls/debug/deps/chat_board-77483abcf2f1a016.rmeta: src/lib.rs
+
+/Users/dowlandaiello/rust/rust-smart-contracts/chat_board/target/rls/debug/deps/chat_board-77483abcf2f1a016.d: src/lib.rs
+
+src/lib.rs:
