4defd52291607f042644469fcc2d2efb49f5ad52
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-22 10:17:58 -0500

    Init crate lib meta.

---
+++ rock_paper_scissors/target/rls/debug/deps/rock_paper_scissors-69b9813cc68effb2.d
@@ -0,0 +1,5 @@
+/Users/dowlandaiello/rust/rust-smart-contracts/rock_paper_scissors/target/rls/debug/deps/rock_paper_scissors-69b9813cc68effb2.rmeta: src/lib.rs
+
+/Users/dowlandaiello/rust/rust-smart-contracts/rock_paper_scissors/target/rls/debug/deps/rock_paper_scissors-69b9813cc68effb2.d: src/lib.rs
+
+src/lib.rs:
