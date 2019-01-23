5702fa45e19baf321d4e5e45c2a2b91dce885658
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-23 08:05:44 -0500

    More linter fixes.

--- rock_paper_scissors/src/lib.rs
+++ rock_paper_scissors/src/lib.rs
@@ -7,7 +7,7 @@ static mut ROUNDS_PLAYED: i32 = 0; // Init rounds
 
 #[no_mangle]
 pub extern "C" fn make_move(move_num: i32) -> i32 {
-    let mut message = "made move".as_bytes(); // Init message buffer
+    let message: &[u8]; // Init message buffer
     let error_message = "player made invalid move!".as_bytes(); // Init err message
     let lost_message = "you lost this round!".as_bytes(); // Init lost message
     let won_message = "you won this round!".as_bytes(); // Init won message
@@ -17,7 +17,7 @@ pub extern "C" fn make_move(move_num: i32) -> i32 {
             0 => message = "played rock".as_bytes(),
             1 => message = "played paper".as_bytes(),
             2 => message = "played scissors".as_bytes(),
-            _ => __ursa_log(error_message.as_ptr(), error_message.len()), // Log error
+            _ => {__ursa_log(error_message.as_ptr(), error_message.len()); return 1}, // Log error
         }
 
         if LAST_MOVE == 10 || ROUNDS_PLAYED == 6 { // Check is starting game or should start game
