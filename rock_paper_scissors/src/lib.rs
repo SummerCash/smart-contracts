6646fdbc976049df8dbdb9166bb8539dd4312c64
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-22 21:17:08 -0500

    Sync...

--- rock_paper_scissors/src/lib.rs
+++ rock_paper_scissors/src/lib.rs
@@ -6,14 +6,31 @@ static mut LAST_MOVE: i32 = 10; // Init last move
 static mut ROUNDS_PLAYED: i32 = 0; // Init rounds
 
 #[no_mangle]
-pub extern "C" fn rock() -> i32 {
-    let message = "played rock".as_bytes();
+pub extern "C" fn make_move(move_num: i32) -> i32 {
+    let message: &[u8]; // Init message
+    let error_message = "player made invalid move!".as_bytes(); // Init err message
+    let lost_message = "you lost this round!".as_bytes(); // Init lost message
+    let won_message = "you won this round!".as_bytes(); // Init won message
 
     unsafe {
-        if LAST_MOVE == 10 { // Check is starting game
-            LAST_MOVE = 0; // Set move
+        match move_num { // Handle move message
+            0 => message = "played rock".as_bytes(),
+            1 => message = "played paper".as_bytes(),
+            2 => message = "played scissors".as_bytes(),
+            _ => __ursa_log(error_message.as_ptr(), error_message.len()), // Log error
+        }
+
+        if LAST_MOVE == 10 || ROUNDS_PLAYED == 6 { // Check is starting game or should start game
             ROUNDS_PLAYED = 0; // Set rounds played
-        }   
+        } else {
+            match LAST_MOVE { // Handle other player moves
+                1 => __ursa_log(lost_message.as_ptr(), lost_message.len()), // Log lost
+                2 => __ursa_log(won_message.as_ptr(), won_message.len()), // Log lost
+                _ => __ursa_log(error_message.as_ptr(), error_message.len()), // Log error
+            }
+        }
+
+        LAST_MOVE = 0; // Set move
     }
 
     unsafe {
